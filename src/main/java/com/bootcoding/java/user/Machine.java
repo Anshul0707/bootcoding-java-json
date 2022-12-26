6b7684c60803c54c8a2b736c37e264c6b1c6259a
Anshul0707 <anshul.markwade786@gmail.com>
2022-12-26 16:40:03 +0530

    files added

---
+++ src/main/java/com/bootcoding/java/user/Machine.java
@@ -0,0 +1,23 @@
+package com.bootcoding.java.user;
+
+public class Machine
+{
+    private String name;
+    private String logicalName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogicalName() {
+        return logicalName;
+    }
+
+    public void setLogicalName(String logicalName) {
+        this.logicalName = logicalName;
+    }
+}
